5bd9eb6097314e855985d422b797958f30813866
Baskaran <baskarandeveloper1423@gmail.com>
2025-12-21 19:42:25 +0530

    feat: add JWT auth middleware and protected routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,8 @@ import { login, signup } from '../controllers/auth.controller.js';
 // Get the validators to check auth data
 import { loginValidator, signupValidator } from '../middlewares/validators/auth.validator.js';
 
+import { protect } from '../middlewares/auth.middleware.js';
+
 // Create a new router for auth routes
 const router = express.Router();
 
@@ -14,5 +16,14 @@ router.post('/signup', signupValidator, signup);
 // Set up the signin route with validation - first check the data, then log in the user
 router.post('/signin', loginValidator, login);
 
+
+// product middleware
+router.get("/me", protect, (req, res) => {
+    return res.status(200).json({
+        sucess: true,
+        message: 'Access granted',
+        user: req.user
+    });
+})
 // Export this router so it can be used in the main app
 export default router;
