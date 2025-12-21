dc2804905e97ec33fcc1d54249b3bde98cae99dd
Baskaran <baskarandeveloper1423@gmail.com>
2025-12-21 15:45:24 +0530

    "chore: commads added for easy to read"

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,23 @@
+// Bring in mongoose to work with MongoDB
 import mongoose from 'mongoose';
 
+// This function connects our app to the MongoDB database
 const connectDB = async () => {
     try {
+        // Try to connect using the URI from environment variables
         const connect = await mongoose.connect(process.env.MONGODB_URI);
 
+        // If it works, log where it's connected
         console.log(`MongoDB connected: ${connect.connection.host}`);
     } catch (error) {
+        // If something goes wrong, log the error
         console.error('MongoDB connection failed');
         console.error(error.message);
 
-        // Crash the app if DB is down
+        // Stop the app if we can't connect to the database
         process.exit(1);
     }
 };
 
+// Export this function so other files can use it
 export default connectDB;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
+// Load environment variables from .env file - this must happen before anything else
 import dotenv from 'dotenv';
-dotenv.config(); // 👈 MUST be first
+dotenv.config(); // MUST be first
 
+// Bring in our main app setup
 import app from './app.js';
+// Get our configuration settings
 import config from './config/index.js';
+// Function to connect to the database
 import connectDB from './config/db.js';
 
+// This function starts everything up
 const startServer = async () => {
+    // First, connect to the database
     await connectDB();
 
+    // Then start the server and listen for requests
     app.listen(config.port, () => {
         console.log(
             `🚀 EventForge server running on port ${config.port} in ${config.env} mode`
@@ -15,4 +22,5 @@ const startServer = async () => {
     });
 };
 
+// Kick off the server startup
 startServer();
